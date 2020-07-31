4df9c9984c3e82ba34989ea8b92017545f238fc6
Serge- <enin.serge@gmail.com>
2020-07-31 10:51:23 +0300

    Transfer, Deval high positioning - fix

--- static/js/jspsych/plugins/jspsych-transfer-test.js
+++ static/js/jspsych/plugins/jspsych-transfer-test.js
@@ -90,18 +90,12 @@ jsPsych.plugins["transfer-test"] = (function() {
 
     var timestamp_onload = jsPsych.totalTime();
 
-     // inject CSS for trial
-     html += '<style id="transfer-test">' +
-      ".vending-machine { margin-top: 5rem; }" +
-      ".jspsych-content { margin: 0; }" +
-      ".jspsych-content-wrapper { display: flex; justify-content: center; }";
-     html += '</style>';
-
     html += `<div id="jspsych-stimulus">
-      <svg class="vending-machine" viewBox="0 0 253 459" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg class="vending-machine" viewBox="0 0 253 459" x="10" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="27" y="20" width="203" height="359" fill="#000"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M253 0V440.506H209.527V459H44.6212V440.506H0V0H253ZM222 279H32V363H222V279ZM59.957 282.531L133.253 309.209L118.546 349.616L45.2501 322.938L59.957 282.531ZM86 210H32V256H86V210ZM154 210H100V256H154V210ZM222 210H168V256H222V210ZM86 148H32V194H86V148ZM154 148H100V194H154V148ZM222 148H168V194H222V148ZM86 86H32V132H86V86ZM154 86H100V132H154V86ZM222 86H168V132H222V86ZM86 24H32V70H86V24ZM154 24H100V70H154V24ZM222 24H168V70H222V24Z" fill="white"/>
       </svg>
+      <div id="transfer-test" class="outcome-container"></div>
     </div>`;
 
 
