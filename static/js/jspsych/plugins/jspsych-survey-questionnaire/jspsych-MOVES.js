a5885d763e898c4a2076a9c78ebe05ad78996766
Serge- <enin.serge@gmail.com>
2020-08-07 23:03:20 +0300

    warning! text removal

--- static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-MOVES.js
+++ static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-MOVES.js
@@ -228,9 +228,6 @@ jsPsych.plugins['MOVES'] = (function () {
               <div class="modal__overlay" tabindex="-1" data-micromodal-close>
                 <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
                   <header class="modal__header">
-                    <h2 class="modal__title" id="modal-1-title">
-                      Warning!
-                    </h2>
                     <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
                   </header>
                   <main class="modal__content" id="modal-1-content">
